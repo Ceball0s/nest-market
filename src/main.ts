bf570941dccc2aded7a46aad515c14f97ff13729
Miguel Angel Ceballos <ceballosmiguel44@gmail.com>
2025-02-01 12:24:12 -0500

    correciones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitar validación global con ValidationPipe
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,  // Ignora campos no definidos en el DTO
+    forbidNonWhitelisted: true,  // Lanza un error si hay campos no permitidos
+    transform: true,  // Convierte los datos a los tipos esperados en el DTO
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+import { CreateUsuarioDto } from './dto/create-usuario.dto';
 
 @Controller('usuarios')
-export class UsuariosController {}
+export class UsuariosController {
+  constructor(private readonly usuariosService: UsuariosService) {}
+
+  @Post()
+  crear(@Body() createUsuarioDto: CreateUsuarioDto) {
+    return this.usuariosService.crear(createUsuarioDto);
+  }
+
+  @Get()
+  obtenerTodos() {
+    return this.usuariosService.obtenerTodos();
+  }
+
+  @Get(':id')
+  obtenerPorId(@Param('id') id: string) {
+    return this.usuariosService.obtenerPorId(id);
+  }
+
+  @Delete(':id')
+  eliminar(@Param('id') id: string) {
+    return this.usuariosService.eliminar(id);
+  }
+}
